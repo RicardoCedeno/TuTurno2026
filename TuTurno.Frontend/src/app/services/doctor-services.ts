54fa2065b12e3eabfb29b54a4d1c5d06074517b4
rcedeno <rcedenom97@gmail.com>
2026-05-21 21:56:07 -0500

    se corrige error en .env

--- TuTurno.Frontend/src/app/services/doctor-services.ts
+++ TuTurno.Frontend/src/app/services/doctor-services.ts
@@ -11,18 +11,18 @@ export class DoctorServices {
   private apiServices = inject(ApiServices);
   //crea un doctor
   createDoctor(doctor: IDoctor): Observable<IResponse<string[]>> {
-    return this.apiServices.post<IResponse<string[]>>('/doctors/createDoctor', doctor, { authenticated: false });
+    return this.apiServices.post<IResponse<string[]>>('/doctors/createDoctor', doctor, { authenticated: true });
   }
   getDoctors(): Observable<IResponse<IDoctor[]>> {
     return this.apiServices.get<IResponse<IDoctor[]>>('/doctors/getAllDoctors', { authenticated: true });
   }
   getDoctorById(id: string): Observable<IResponse<IDoctor>> {
-    return this.apiServices.get<IResponse<IDoctor>>(`/doctors/getDoctorById/${id}`, { authenticated: false });
+    return this.apiServices.get<IResponse<IDoctor>>(`/doctors/getDoctorById/${id}`, { authenticated: true });
   }
   updateDoctor(doctor: IDoctor): Observable<IResponse<string[]>> {
-    return this.apiServices.put<IResponse<string[]>>(`/doctors/updateDoctor/${doctor.id}`, doctor, { authenticated: false });
+    return this.apiServices.put<IResponse<string[]>>(`/doctors/updateDoctor/${doctor.id}`, doctor, { authenticated: true });
   }
   deleteDoctor(id: string): Observable<IResponse<string[]>> {
-    return this.apiServices.delete<IResponse<string[]>>(`/doctors/deleteDoctor/${id}`, { authenticated: false });
+    return this.apiServices.delete<IResponse<string[]>>(`/doctors/deleteDoctor/${id}`, { authenticated: true });
   }
 }
